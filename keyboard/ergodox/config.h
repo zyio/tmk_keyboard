d2427380e3d82df0f9b543615c27f9d823a09306
Oleg Kostyuk <cub.uanic@gmail.com>
2014-03-30 05:49:01 +0300

    Update of IS_COMMAND (thanks to ibm4704 branch)

--- keyboard/ergodox/config.h
+++ keyboard/ergodox/config.h
@@ -60,6 +60,7 @@ Project located at <https://github.com/benblazak/ergodox-firmware>
 
 /* key combination for command */
 #define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LCTL) | MOD_BIT(KC_RCTL)) || \
     keyboard_report->mods == (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT)) \
 )
 
